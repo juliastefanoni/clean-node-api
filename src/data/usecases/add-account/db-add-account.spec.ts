fa741e3f5d2e37e5904df1d4991e561017603048
juliastefanoni <juliastefanonidev@gmail.com>
2023-03-23 17:55:40 -0300

    test: ensure DbAddAccount retuns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -110,4 +110,25 @@ describe('Db AddAccount UseCase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual(
+      {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'hashed_password'
+      }
+    )
+  })
 })
