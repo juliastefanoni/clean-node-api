8bbdb1b1ba69603f046dd9d27a4fa50f7434db84
juliastefanoni <juliastefanonidev@gmail.com>
2023-03-29 16:19:53 -0300

    feat: add SignUp factory

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/no-namespace': 'off'
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 }
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
-import { Response, Request, Router } from 'express'
+
+import { Router } from 'express'
+import { AdaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
-  router.post('/signup', (req: Request, res: Response) => {
-    res.json({ ok: '' })
-  })
+  router.post('/signup', AdaptRoute(makeSignUpController()))
 }
