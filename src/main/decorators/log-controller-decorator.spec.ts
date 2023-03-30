57f246da6b38f6a4adef37bbd151f3ce0a0e91ac
juliastefanoni <juliastefanonidev@gmail.com>
2023-03-30 11:38:14 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -53,4 +53,26 @@ describe('Log Decorator', () => {
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Júlia'
+      }
+    })
+  })
 })
